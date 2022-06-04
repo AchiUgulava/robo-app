73e9cfaf42cb4023e934dd8c030499508a13dd4d
Achi <achiugulava@gmail.com>
2022-06-04 21:14:36 +0400

    updated package.json

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <footer className="z-40 flex flex-wrap items-center justify-center w-full px-4 mb-4 realtive max-h-36">
   <small className="w-full text-xl text-center">© 2022 <b >Achi Ugulava</b>. All Rights Reserved</small>
   <div className="w-full text-xl text-center">
-    <a href="#" title="Language" className="">Language</a>
-    <a href="#"    title="Terms" className="">Terms of Use</a>
-    <a href="#"  title="Privacy" className="">Privacy</a>
+    <a  title="Language" className="">Language</a>
+    <a  title="Terms" className="">Terms of Use</a>
+    <a  title="Privacy" className="">Privacy</a>
   </div>
 </footer>
   );
